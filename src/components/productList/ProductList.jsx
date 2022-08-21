135fdc50b897ffd958918d49d1ca45ed933e0bd8
Huzaifa Rehman <60300174+Huzaifa246@users.noreply.github.com>
2022-08-21 13:25:56 -0700

    Add files via upload

---
+++ src/components/productList/ProductList.jsx
@@ -0,0 +1,23 @@
+import "./productList.css";
+import Product from "../product/Product";
+import { products } from "../../data";
+
+const ProductList = () => {
+  return (
+    <div className="pl">
+      <div className="pl-texts">
+        <h1 className="pl-title">Where there is no struggle there is no progress</h1>
+        <p className="pl-desc">
+          Projects and work coming soon!!!
+        </p>
+      </div>
+      <div className="pl-list">
+        {products.map((item) => (
+          <Product key={item.id} img={item.img} link={item.link} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
